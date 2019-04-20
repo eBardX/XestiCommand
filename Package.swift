7ca425d5e604a41765392f4f2aa0ad1d74ea22ee
J. G. Pusey <ebardx@gmail.com>
2019-04-20 15:19:09 -0400

    Updated to support Swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 // © 2018 J. G. Pusey (see LICENSE.md)
 
@@ -8,8 +8,8 @@ let package = Package(name: "XestiCommand",
                       products: [.library(name: "XestiCommand",
                                           targets: ["XestiCommand"])],
                       dependencies: [.package(url: "https://github.com/eBardX/XestiPath.git",
-                                              from: "1.0.0"),
+                                              from: "1.0.1"),
                                      .package(url: "https://github.com/eBardX/XestiText.git",
-                                              from: "1.1.0")],
+                                              from: "1.1.1")],
                       targets: [.target(name: "XestiCommand",
                                         dependencies: ["XestiPath", "XestiText"])])
